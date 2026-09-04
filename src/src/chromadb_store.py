bdaa56bd5c92aa98591671382c84afb46bac36a4
Poorvika-bv3 <poorvikabv117@gmail.com>
2026-09-04 21:54:33 +0530

    Update chromadb_store.py

--- src/src/chromadb_store.py
+++ src/src/chromadb_store.py
@@ -1,1 +1,27 @@
 
+import chromadb
+
+client = chromadb.PersistentClient(
+    path="./chroma_db"
+)
+
+collection = client.get_or_create_collection(
+    name="research_papers"
+)
+
+def store_chunks(chunks, embeddings):
+    ids = [f"chunk_{i}" for i in range(len(chunks))]
+
+    # Prevent duplicate-ID error if you run test twice
+    try:
+        collection.delete(ids=ids)
+    except Exception:
+        pass
+
+    collection.add(
+        ids=ids,
+        embeddings=embeddings,
+        documents=chunks
+    )
+
+    print(f"Stored {len(chunks)} chunks in ChromaDB.")
