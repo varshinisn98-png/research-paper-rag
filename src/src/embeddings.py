e8860b54a9191f9137615b161d263ba0f11916ba
Akshatha TM <akshathatm2330@gmail.com>
2026-09-04 22:33:17 +0530

    Test and improve embedding generation

--- src/src/embeddings.py
+++ src/src/embeddings.py
@@ -1,1 +1,14 @@
+from sentence_transformers import SentenceTransformer
 
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+
+def create_embeddings(chunks):
+    embeddings = model.encode(chunks)
+    return embeddings.tolist()
+
+
+def create_query_embedding(question):
+    embedding = model.encode([question])
+    return embedding.tolist()[0]
